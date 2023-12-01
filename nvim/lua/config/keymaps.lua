2d44f1a2383d4819ea8b3b7768b033b44ea6a315
quy.truong <quy.truong@saigontechnology.com>
2023-12-01 12:14:19 +0700

    Update special workspace

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+vim.keymap.set("n", "<leader>gb", ":Git blame_line<CR>", { desc = "Blame this line" })
+vim.keymap.set("n", "<leader>gB", function()
+  local Util = require("lazyvim.util")
+  -- local util = require("lazy.util")
+  Util.terminal({ "tig", "blame", vim.fn.expand("%") }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
+end, { desc = "Open tig blame" })
+
+vim.keymap.set("n", "<leader>wc", function()
+  require("dbee").open()
+end, { desc = "Open dbee" })
