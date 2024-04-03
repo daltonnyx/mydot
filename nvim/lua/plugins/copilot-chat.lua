3db03fbd440f866136ffd19679cadfc71a01cb2d
quy.truong <quy.truong@saigontechnology.com>
2024-04-03 14:54:50 +0700

    add yt-player with rofi

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false, -- Enable debugging
+      -- See Configuration section for rest
+      window = {
+        layout = "float",
+        relative = "cursor",
+        width = 1,
+        height = 0.4,
+        row = 1,
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
---
+++ rofi/yt-player/yt-player.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+
+main() {
+	local play_menu="Play now"
+	local search_menu="Search"
+	local end_menu="End"
+
+	local option=$(echo -e "$play_menu\n$search_menu\n$end_menu" | rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Youtube: ")
+
+	case $option in
+	"Play now")
+		local title=$(rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Play now: ")
+		pkill -f "mpv-bg"
+		mpv --no-audio-display --ytdl-format="bestvideo[height<2500]+bestaudio" "ytdl://ytsearch:$title" --loop --vo=gpu-next --wayland-app-id="mpv-bg"
+		;;
+	"Search")
+		local search=$(rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Search: ")
+		local play=$(yt-dlp --get-title "ytsearch10:$search" --no-warnings --flat-playlist --skip-download --quiet --no-check-certificate --geo-bypass | rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Play: ")
+		mpv --no-audio-display --ytdl-format="bestvideo[height<2500]+bestaudio" "ytdl://ytsearch:$play" --loop --vo=gpu-next --wayland-app-id="mpv-bg"
+		;;
+	"End") pkill -f "mpv-bg" ;;
+	*) ;;
+	esac
+}
+
+main
