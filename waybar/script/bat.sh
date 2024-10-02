e58a281d3dcfbe9474aed933c38937bc1339c3a7
quy.truong <quy.truong@saigontechnology.com>
2024-10-02 08:55:14 +0700

    update main config

--- waybar/script/bat.sh
+++ waybar/script/bat.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-devices=('F3:2E:12:1C:32:F6' 'DE:F1:36:6E:87:E8')
+devices=('F3:2E:12:1C:32:F9' 'DE:F1:36:6E:87:E8')
 icons=('󰥻' '󰍽')
 
 for ((i = 0; i < ${#devices[@]}; i++)); do
-	percent=$(bluetoothctl info ${devices[i]} | grep -oP "Battery Percentage: .+\(\K.+(?=\))")
-	if [ -z "$percent" ]; then
-		percent="0"
-	fi
-	echo -n "$percent% ${icons[i]}"
-	if [ $i -ne $((${#devices[@]} - 1)) ]; then
-		echo -n " "
-	fi
+  percent=$(bluetoothctl info ${devices[i]} | grep -oP "Battery Percentage: .+\(\K.+(?=\))")
+  if [ -z "$percent" ]; then
+    percent="0"
+  fi
+  echo -n "$percent% ${icons[i]}"
+  if [ $i -ne $((${#devices[@]} - 1)) ]; then
+    echo -n " "
+  fi
 done
