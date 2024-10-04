6d77aa847da012812f43b748417cc02213ba6232
quy.truong <quy.truong@saigontechnology.com>
2024-10-04 15:31:21 +0700

    update background video

--- rofi/yt-player/yt-player.sh
+++ rofi/yt-player/yt-player.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #
-
+reset() {
+  pkill -f "mpvpaper"
+  pkill -f "mpv"
+  if [[ $1 == "music" ]]; then
+    return
+  fi
+  pkill -f "hyprpaper"
+}
 main() {
   local play_menu="Play now"
   local search_menu="Search"
@@ -11,15 +18,36 @@ main() {
   case $option in
   $play_menu)
     local title=$(rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Play now: ")
-    pkill -f "mpvpaper"
-    pkill -f "hyprpaper"
+    reset
     mpvpaper --mpv-options="--no-audio-display --loop --ytdl-format=\"bestvideo[height<1081]+bestaudio\"" HDMI-A-1 "ytdl://ytsearch:$title"
     ;;
   $search_menu)
     local search=$(rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Search: ")
-    local play=$(yt-dlp --get-title "ytsearch10:$search" --no-warnings --flat-playlist --skip-download --quiet --no-check-certificate --geo-bypass | rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Play: ")
-    pkill -f "hyprpaper"
-    pkill -f "mpvpaper"
+    local i=0
+    local titles=()
+    local urls=()
+    while IFS= read -r link; do
+      if [ $((i % 2)) -eq 0 ]; then
+        titles+=("$link")
+      else
+        urls+=("$link")
+      fi
+      i=$((i + 1))
+    done < <(yt-dlp --get-url --get-title "https://www.youtube.com/results?search_query=$search" --no-warnings --skip-download --quiet --no-check-certificate --geo-bypass --playlist-end 15 --flat-playlist)
+    local printedtitles=$(
+      IFS=$'\n'
+      echo "${titles[*]}"
+    )
+    local play=$(echo "$printedtitles" | rofi -theme catppuccin-macchiato -show-icons -icon-theme 'Papirus' -dmenu -p "Play: ")
+    local index=0
+    for title in "${titles[@]}"; do
+      if [ "$title" == "$play" ]; then
+        break
+      fi
+      index=$((index + 1))
+    done
+    reset
+
     mpvpaper --mpv-options="--no-audio-display --loop --ytdl-format=\"bestvideo[height<1081]+bestaudio\"" HDMI-A-1 "ytdl://ytsearch:$play"
     ;;
   $music_menu)
@@ -47,13 +75,11 @@ main() {
       fi
       index=$((index + 1))
     done
-    pkill -f "mpvpaper"
-    pkill -f "mpv"
+    reset music
     mpv --no-audio-display --ytdl-format="bestaudio" "${urls[$index]}"
     ;;
   $end_menu)
-    pkill -f "mpvpaper"
-    pkill -f "mpv"
+    reset
     pgrep hyprpaper || hyprpaper
     ;;
   *) ;;
