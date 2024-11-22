3e37a395117d19c7e074f1f093146b9fedcb7c70
quy.truong <quy.truong@saigontechnology.com>
2024-11-22 14:41:28 +0700

    update hyprland

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -13,18 +13,18 @@ opt.scrolloff = 8
 
 if vim.env.SSH_TTY then
   opt.clipboard:append("unnamedplus")
-  local function paste()
-    return vim.split(vim.fn.getreg(""), "\n")
-  end
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = paste,
-      ["*"] = paste,
-    },
-  }
+  -- local function paste()
+  --   return vim.split(vim.fn.getreg(""), "\n")
+  -- end
+  -- vim.g.clipboard = {
+  --   name = "OSC 52",
+  --   copy = {
+  --     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+  --     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  --   },
+  --   paste = {
+  --     ["+"] = paste,
+  --     ["*"] = paste,
+  --   },
+  -- }
 end
