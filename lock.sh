85b057d9bb860082043a4cf34027b4f3da44a6b2
Quy Truong <quytruong851@gmail.com>
2025-01-02 21:08:28 +0700

    update config

--- lock.sh
+++ lock.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
-scrot -o /tmp/screenshot.png --silent
-convert -scale 10% -blur 0x1.5 -resize 1000% /tmp/screenshot.png /tmp/screenshotblur.png
 if [ -n "$WAYLAND_DISPLAY" ]; then
-	pgrep swaylock || swaylock -C /home/quy.truong/desktop-config/mydot/swaylock-macchiato.conf -f --screenshots --clock --indicator --fade-in 0.1 --effect-blur 7x7 --indicator-thickness 4 #-i /home/quy.truong/desktop-config/mydot/awesome/themes/powerarrow-dark/wall.png
+  wall=$(find $HOME/Wallpapers/ -type f | shuf -n 1)
+  killall swaylock -q
+  swaylock -C /home/quy.truong/desktop-config/mydot/swaylock-macchiato.conf -f --fade-in 0.3 --clock --indicator --indicator-thickness 4 -i $wall
+  #pgrep hyprlock || hyprlock
 else
-	scrot -o /tmp/screenshot.png --silent
-	convert -scale 10% -blur 0x1.5 -resize 1000% /tmp/screenshot.png /tmp/screenshotblur.png
-	i3lock -i /tmp/screenshotblur.png
+  BLANK='#00000000'
+  CLEAR='#f4dbd622'
+  DEFAULT='#24273acc'
+  TEXT='#cad3f5ee'
+  WRONG='#f4dbd6bb'
+  VERIFYING='#8aadf4bb'
+
+  i3lock \
+    --insidever-color=$CLEAR \
+    --ringver-color=$VERIFYING \
+    \
+    --insidewrong-color=$CLEAR \
+    --ringwrong-color=$WRONG \
+    \
+    --inside-color=$BLANK \
+    --ring-color=$BLANK \
+    --line-color=$BLANK \
+    --separator-color=$DEFAULT \
+    \
+    --verif-color=$TEXT \
+    --wrong-color=$TEXT \
+    --time-color=$TEXT \
+    --date-color=$TEXT \
+    --layout-color=$TEXT \
+    --keyhl-color=$WRONG \
+    --bshl-color=$WRONG \
+    \
+    --screen 1 \
+    --blur 7 \
+    --clock \
+    --indicator \
+    --time-str="%H:%M:%S" \
+    --date-str="%A, %Y-%m-%d" \
+    --keylayout 0
+  # scrot -o /tmp/screenshot.png --silent
+  # convert -scale 10% -blur 0x1.5 -resize 1000% /tmp/screenshot.png /tmp/screenshotblur.png
+  # i3lock -i /tmp/screenshotblur.png
 fi
