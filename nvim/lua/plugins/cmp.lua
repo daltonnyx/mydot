dfd9d7041f9541db6fc5ee02a441538abb3e40d7
Quy Truong <quytruong851@gmail.com>
2025-01-23 21:55:40 +0700

    migrate to avante

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,7 +1,70 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    keymap = { preset = "super-tab" },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      snippets = {
+        expand = function(snippet, _)
+          return LazyVim.cmp.expand(snippet)
+        end,
+      },
+      appearance = {
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release, assuming themes add support
+        use_nvim_cmp_as_default = false,
+        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        accept = {
+          -- experimental auto-brackets support
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          enabled = vim.g.ai_cmp,
+        },
+      },
+
+      -- experimental signature help support
+      -- signature = { enabled = true },
+
+      sources = {
+        -- adding any nvim-cmp sources here will enable them
+        -- with blink.compat
+        compat = { "avante_commands", "avante_mentions", "avante_files" },
+        default = { "lsp", "path", "snippets", "buffer" },
+        cmdline = {},
+      },
+
+      keymap = {
+        preset = "super-tab",
+        ["<C-y>"] = { "select_and_accept" },
+      },
+    },
+  },
+  {
+    "saghen/blink.compat",
+    lazy = true,
+    opts = {},
+    config = function()
+      -- monkeypatch cmp.ConfirmBehavior for Avante
+      require("cmp").ConfirmBehavior = {
+        Insert = "insert",
+        Replace = "replace",
+      }
+    end,
   },
 }
 -- return {
--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,22 +1,24 @@
-return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-      window = {
-        layout = "float",
-        relative = "cursor",
-        width = 0.8,
-        height = 0.8,
-        row = 1,
-      },
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-}
+-- return {
+--   {
+--     "CopilotC-Nvim/CopilotChat.nvim",
+--     branch = "canary",
+--     dependencies = {
+--       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+--       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+--     },
+--     opts = {
+--       debug = true, -- Enable debugging
+--       -- See Configuration section for rest
+--       window = {
+--         layout = "float",
+--         relative = "cursor",
+--         width = 0.8,
+--         height = 0.8,
+--         row = 1,
+--       },
+--     },
+--     -- See Commands section for default commands if you want to lazy load on them
+--   },
+-- }
+--
+return {}
