70119c81873359398b45837a708ff9043e644d5f
Quy Truong <quytruong851@gmail.com>
2025-09-03 09:52:15 +0700

    update hyprland config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.cmd.colorscheme("catppuccin-macchiato")
+vim.cmd.colorscheme("catppuccin")
 -- Disable autoformat for markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "md" },
