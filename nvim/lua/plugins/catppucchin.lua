e1bb76a75923c8bf0bbd4ef4852e861f4a5e4b96
Quy Truong <quytruong851@gmail.com>
2025-09-15 10:30:17 +0700

    update dotfiles

--- nvim/lua/plugins/catppucchin.lua
+++ nvim/lua/plugins/catppucchin.lua
@@ -3,8 +3,12 @@ return {
   name = "catppuccin",
   priority = 1000,
   ---@class CatppuccinOptions
-  opts = {
-    flavour = "macchiato",
-    transparent_background = true,
-  },
+  opts = function(_, opts)
+    local module = require("catppuccin.groups.integrations.bufferline")
+    if module then
+      module.get = module.get_theme
+    end
+    opts.transparent_background = true
+    return opts
+  end,
 }
